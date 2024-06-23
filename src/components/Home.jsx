8f55058ead22a517649b61a9f8a9873ed373452a
riteshkv57 <ritesh0057@gmail.com>
2024-06-24 00:40:09 +0530

    update

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+    <div className='home'>
+      <h1>Welcome to Our Website!</h1>
+      <p>This is the home page of our website.</p>
+    </div>
+    <div className='home'>
+    <h1>Welcome to Our Website!</h1>
+    <p>This is the home page of our website.</p>
+  </div>
+  <div className='home'>
+    <h1>Welcome to Our Website!</h1>
+    <p>This is the home page of our website.</p>
+  </div>
+  <div className='home'>
+    <h1>Welcome to Our Website!</h1>
+    <p>This is the home page of our website.</p>
+  </div>
+  </>
+  );
+};
+
+export default Home;
